08f0b94f61e33a434edcf84316aefe9993db89f4
TheUnicorn2026 <theunicorn2026@gmail.com>
2026-01-07 04:32:48 +0000

    Update models.py
    added paranthesis in plan models

--- myproject/plan/models.py
+++ myproject/plan/models.py
@@ -7,6 +7,6 @@ from django.db import models
 class Plan(models.Model):
     
     Type = models.CharField(max_length = 20)
-    StartDate = models.DateField
-    Duration = models.IntegerField
+    StartDate = models.DateField()
+    Duration = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add = True)
